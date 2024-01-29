ea36728945acef1de2a04d5775ebdb13d43c1c3e
Anish949 <74363280+Anish949@users.noreply.github.com>
2024-01-29 12:35:02 +0545

    navbar handshake icon

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <>
-    <div className="fcontainer">
-     <div className ="logo">Logo</div>
-     <div className ="menu">
-     <div className ="home">Home</div>
-     <div className ="services">Services</div>
-     <div className ="joinus">Join Us</div>
-     <div className ="blog">Blog</div>
-     <div className ="aboutus">About Us</div>
-     <div className="cocreate">
-     <div className="cocreatewithus">CO-CREATE WITH US</div>
-     </div>
-     </div>
-    </div>
-
+      <div className="fcontainer">
+        <div className="logo">Logo</div>
+        <div className="menu">
+          <div className="home">Home</div>
+          <div className="services">Services</div>
+          <div className="joinus">Join Us</div>
+          <div className="blog">Blog</div>
+          <div className="aboutus">About Us</div>
+          <div className="cocreate">
+            <div className="cocreatewithus">
+              <img src="/handshake.png" alt="handshake" className="handshake" />
+              CO-CREATE WITH US
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="main">dsfhadsjf asklfj aklsjf</div>
     </>
   );
 }
 
 export default App;
-
-    

0970d2a1e1f87a478c961373231cd356872a46be
Anish949 <74363280+Anish949@users.noreply.github.com>
2024-01-29 12:51:48 +0545

    handshake icon

--- src/App.js
+++ src/App.js
@@ -4,20 +4,22 @@ import "./App.css";
 function App() {
   return (
     <>
-    <div className="fcontainer">
-     <div className ="logo">Logo</div>
-     <div className ="menu">
-     <div className ="home">Home</div>
-     <div className ="services">Services</div>
-     <div className ="joinus">Join Us</div>
-     <div className ="blog">Blog</div>
-     <div className ="aboutus">About Us</div>
-     <div className="cocreate">
-     <div className="cocreatewithus">CO_CREATE WITH US</div>
-     </div>
-     </div>
-    </div>
-
+      <div className="fcontainer">
+        <div className="logo">Logo</div>
+        <div className="menu">
+          <div className="home">Home</div>
+          <div className="services">Services</div>
+          <div className="joinus">Join Us</div>
+          <div className="blog">Blog</div>
+          <div className="aboutus">About Us</div>
+          <div className="cocreate">
+            <div className="cocreatewithus">
+              <img src="/handshake.png" alt="handshake" className="handshake" />
+              CO_CREATE WITH US
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }

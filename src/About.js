a8cbe3f16faf86c56347db657794d22c3b97c54d
Anish949 <74363280+Anish949@users.noreply.github.com>
2024-02-16 15:25:13 +0545

    about section

--- src/About.js
+++ src/About.js
@@ -3,46 +3,81 @@ import "./About.css";
 const About = () => {
   return (
     <>
-   <div className="fone">
-   <div className="aboutone"><br/>About Us
-   <p className="aboutsys"><br/>Established in 2006 A.D., Systematic Software is the first Software
-    Development Center in Western<br/>Nepal, located in Butwal. We specialize in providing
-     advanced technology support, creating customized<br/>application packages for various
-     professional institutions and organizations. 
-     <br/>
-     <br/>
-
-     With a track record of success, we've collaborated with financial institutions, 
-     investment companies,<br/> educational sectors, automobile industries, hospitals, 
-     and more. Thousands of users have chosen <br/>Systematic Software as their trusted 
-     technology partner.
-     <br/>
-     <br/>
-     <br/>
-    <span className="blue-text">Join us in building success through innovation and reliability
-    <img src="bluearrow.svg"
-     alt="bluearrow"
-      className="bluearrow"
-     /></span>
-    </p>
-   </div>
-   </div>
-   <div>
-  <div className="coreframe">hi</div>
-  </div>
-
-    
-    
-    
-    
-    
-    
+      <main className="">
+        <div className="fone">
+          <div className="aboutone">
+            <br />
+            About Us
+            <p className="aboutsys">
+              <br />
+              Established in 2006 A.D., Systematic Software is the first
+              Software Development Center in Western
+              <br />
+              Nepal, located in Butwal. We specialize in providing advanced
+              technology support, creating customized
+              <br />
+              application packages for various professional institutions and
+              organizations.
+              <br />
+              <br />
+              With a track record of success, we've collaborated with financial
+              institutions, investment companies,
+              <br /> educational sectors, automobile industries, hospitals, and
+              more. Thousands of users have chosen <br />
+              Systematic Software as their trusted technology partner.
+              <br />
+              <br />
+              <br />
+              <span className="blue-text">
+                Join us in building success through innovation and reliability
+                <img
+                  src="bluearrow.svg"
+                  alt="bluearrow"
+                  className="bluearrow"
+                />
+              </span>
+            </p>
+          </div>
+        </div>
+        <div className="ftwo">
+          <div className="boxtwo"></div>
+          <div className="abouttwo">
+            <br />
+            About Us
+            <p className="aboutsys">
+              <br />
+              Established in 2006 A.D., Systematic Software is the first
+              Software Development Center in Western
+              <br />
+              Nepal, located in Butwal. We specialize in providing advanced
+              technology support, creating customized
+              <br />
+              application packages for various professional institutions and
+              organizations.
+              <br />
+              <br />
+              With a track record of success, we've collaborated with financial
+              institutions, investment companies,
+              <br /> educational sectors, automobile industries, hospitals, and
+              more. Thousands of users have chosen <br />
+              Systematic Software as their trusted technology partner.
+              <br />
+              <br />
+              <br />
+              <span className="blue-text">
+                Join us in building success through innovation and reliability
+                <img
+                  src="bluearrow.svg"
+                  alt="bluearrow"
+                  className="bluearrow"
+                />
+              </span>
+            </p>
+          </div>
+        </div>
+      </main>
     </>
-
-
-
-  )
-  
+  );
 };
 
 export default About;

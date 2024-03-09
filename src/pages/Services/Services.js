8892f80781d06ea8ad6a23109df1ebd157d1752d
Sanskar <sskrlmsl@gmail.com>
2024-03-09 11:55:15 +0545

    Services Section

--- src/pages/Services/Services.js
+++ src/pages/Services/Services.js
@@ -1,13 +1,183 @@
 import React from "react";
 import NavBar from "../../components/global/nav";
 import Footer from "../../components/global/footer";
+import "./services.css";
 
 export default function Services() {
   return (
     <>
       <NavBar />
-      <section id="services" className="main">
-        
+      <section id="services" className="services">
+        <div className="services_container">
+          <div className="product_container">
+            <a className="product" href="#school">
+              <div className="productImage">
+                <img src="/assets/images/products/school.png" alt="school" />
+              </div>
+              <h3>School and College</h3>
+            </a>
+            <a className="product" href="#cooperative">
+              <div className="productImage">
+                <img
+                  src="/assets/images/products/cooperative.png"
+                  alt="cooperative"
+                />
+              </div>
+              <h3>Co-operative Software</h3>
+            </a>
+            <a className="product" href="#hospital">
+              <div className="productImage">
+                <img
+                  src="/assets/images/products/hospital.png"
+                  alt="hospital"
+                />
+              </div>
+              <h3>Hospital Management</h3>
+            </a>
+            <a className="product" href="#lab">
+              <div className="productImage">
+                <img src="/assets/images/products/lab.png" alt="lab" />
+              </div>
+              <h3>Lab Management</h3>
+            </a>
+            <a className="product" href="#business">
+              <div className="productImage">
+                <img
+                  src="/assets/images/products/business.png"
+                  alt="business"
+                />
+              </div>
+              <h3>Business Softwares</h3>
+            </a>
+          </div>
+        </div>
+        <div className="products_description">
+          <div className="products_description_container" id="school">
+            <div className="content">
+              <h2>School and Colleges</h2>
+              <p>
+                The Systematic Schooling Software is equipped with user-friendly
+                dashboards, providing login access for administrators and staff.
+                This comprehensive software encompasses various modules that
+                seamlessly manage all aspects of your educational institution.
+                From student admissions to the issuance of transfer certificates
+                upon program completion, the software handles crucial functions
+                efficiently. Modules dedicated to Examinations, Gradebooks,
+                Hostel, Library, Transportation, and more ensure a well-rounded
+                approach to school management. Moreover, it includes a
+                fully-fledged Human Resource module for staff management and a
+                Finance module to plan and allocate different fee structures for
+                students.
+              </p>
+            </div>
+            <div className="imageContainer">
+              <img
+                src="https://1.bp.blogspot.com/-7dVEfo-4z5w/YMi1T2TCzMI/AAAAAAAABoQ/lXMdAU8tAt0dJl4lUArP8ENfsob3Te62QCLcBGAsYHQ/s1335/IMG_20210615_171007.jpg"
+                alt="school"
+              />
+            </div>
+          </div>
+          <div className="products_description_container" id="cooperative">
+            <div className="content">
+              <h2>School and Colleges</h2>
+              <p>
+                The Systematic Schooling Software is equipped with user-friendly
+                dashboards, providing login access for administrators and staff.
+                This comprehensive software encompasses various modules that
+                seamlessly manage all aspects of your educational institution.
+                From student admissions to the issuance of transfer certificates
+                upon program completion, the software handles crucial functions
+                efficiently. Modules dedicated to Examinations, Gradebooks,
+                Hostel, Library, Transportation, and more ensure a well-rounded
+                approach to school management. Moreover, it includes a
+                fully-fledged Human Resource module for staff management and a
+                Finance module to plan and allocate different fee structures for
+                students.
+              </p>
+            </div>
+            <div className="imageContainer">
+              <img
+                src="https://1.bp.blogspot.com/-7dVEfo-4z5w/YMi1T2TCzMI/AAAAAAAABoQ/lXMdAU8tAt0dJl4lUArP8ENfsob3Te62QCLcBGAsYHQ/s1335/IMG_20210615_171007.jpg"
+                alt="school"
+              />
+            </div>
+          </div>
+          <div className="products_description_container" id="hospital">
+            <div className="content">
+              <h2>Hospital Management</h2>
+              <p>
+                The Systematic Schooling Software is equipped with user-friendly
+                dashboards, providing login access for administrators and staff.
+                This comprehensive software encompasses various modules that
+                seamlessly manage all aspects of your educational institution.
+                From student admissions to the issuance of transfer certificates
+                upon program completion, the software handles crucial functions
+                efficiently. Modules dedicated to Examinations, Gradebooks,
+                Hostel, Library, Transportation, and more ensure a well-rounded
+                approach to school management. Moreover, it includes a
+                fully-fledged Human Resource module for staff management and a
+                Finance module to plan and allocate different fee structures for
+                students.
+              </p>
+            </div>
+            <div className="imageContainer">
+              <img
+                src="https://1.bp.blogspot.com/-7dVEfo-4z5w/YMi1T2TCzMI/AAAAAAAABoQ/lXMdAU8tAt0dJl4lUArP8ENfsob3Te62QCLcBGAsYHQ/s1335/IMG_20210615_171007.jpg"
+                alt="school"
+              />
+            </div>
+          </div>
+          <div className="products_description_container" id="lab">
+            <div className="content">
+              <h2>Lab Management</h2>
+              <p>
+                The Systematic Schooling Software is equipped with user-friendly
+                dashboards, providing login access for administrators and staff.
+                This comprehensive software encompasses various modules that
+                seamlessly manage all aspects of your educational institution.
+                From student admissions to the issuance of transfer certificates
+                upon program completion, the software handles crucial functions
+                efficiently. Modules dedicated to Examinations, Gradebooks,
+                Hostel, Library, Transportation, and more ensure a well-rounded
+                approach to school management. Moreover, it includes a
+                fully-fledged Human Resource module for staff management and a
+                Finance module to plan and allocate different fee structures for
+                students.
+              </p>
+            </div>
+            <div className="imageContainer">
+              <img
+                src="https://1.bp.blogspot.com/-7dVEfo-4z5w/YMi1T2TCzMI/AAAAAAAABoQ/lXMdAU8tAt0dJl4lUArP8ENfsob3Te62QCLcBGAsYHQ/s1335/IMG_20210615_171007.jpg"
+                alt="school"
+              />
+            </div>
+          </div>
+          <div className="products_description_container" id="business">
+            <div className="content">
+              <h2>Business Softwares</h2>
+              <p>
+                The Systematic Schooling Software is equipped with user-friendly
+                dashboards, providing login access for administrators and staff.
+                This comprehensive software encompasses various modules that
+                seamlessly manage all aspects of your educational institution.
+                From student admissions to the issuance of transfer certificates
+                upon program completion, the software handles crucial functions
+                efficiently. Modules dedicated to Examinations, Gradebooks,
+                Hostel, Library, Transportation, and more ensure a well-rounded
+                approach to school management. Moreover, it includes a
+                fully-fledged Human Resource module for staff management and a
+                Finance module to plan and allocate different fee structures for
+                students.
+              </p>
+            </div>
+            <div className="imageContainer">
+              <img
+                src="https://1.bp.blogspot.com/-7dVEfo-4z5w/YMi1T2TCzMI/AAAAAAAABoQ/lXMdAU8tAt0dJl4lUArP8ENfsob3Te62QCLcBGAsYHQ/s1335/IMG_20210615_171007.jpg"
+                alt="school"
+              />
+            </div>
+          </div>
+        </div>
       </section>
       <Footer />
     </>
